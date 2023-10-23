1d41dd9e792d2e3a4d243b60c08845b0dce1530d
John Owens <john.owens@opencastsoftware.com>
2023-10-23 15:35:45 +0100

    Updated unit tests

--- NewsPlay/test/services/NewsServiceTest.scala
+++ NewsPlay/test/services/NewsServiceTest.scala
@@ -9,7 +9,15 @@ class NewsServiceTest extends AnyFunSuiteLike {
     val newsArticles = NewsService.GetNews();
 
     assert(newsArticles.nonEmpty)
+  }
 
+  test("Check if news contains this title") {
+    val newsArticles = NewsService.GetNews();
+
+    var expectedNewsTitle = "Marvel movie filming in Edinburgh"
+    val results = newsArticles.find(item => item.toLowerCase() === expectedNewsTitle.toLowerCase())
+
+    assert(results !== null)
   }
 
 }
