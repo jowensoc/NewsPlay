944c0a9a32686d38785b2472855b3a82b10c1e52
John Owens <114502866+jowensoc@users.noreply.github.com>
2023-11-01 09:29:13 +0000

    Added new data service (#14)

--- NewsPlay/app/services/NewsService.scala
+++ NewsPlay/app/services/NewsService.scala
@@ -5,22 +5,12 @@ import models.{NewsArticle, SearchParameters}
 
 object NewsService {
 
+  private val dataService = new NewsDataService("test")
   private val listOfNewsArticles = initialiseArticles()
+  private var environmentName = ""
 
   def initialiseArticles(): ListBuffer[NewsArticle] = {
-    var list = ListBuffer[NewsArticle]()
-
-    list.addOne(new NewsArticle("Jamie wins trip to New York", "Grover", list.size + 1))
-    list.addOne(new NewsArticle("Glasgow to host Eurovision", "Bert", list.size + 1))
-    list.addOne(new NewsArticle("Marvel movie filming in Edinburgh", "Ernie", list.size + 1))
-    list.addOne(new NewsArticle("Warm weather heads to Scotland", "Big Bird", list.size + 1))
-
-    list.addOne(new NewsArticle("Asteroid hits local town. Everyone is fine", "Grover", list.size + 1))
-    list.addOne(new NewsArticle("Toast always lands on buttered side, scientist finds", "Bert", list.size + 1))
-    list.addOne(new NewsArticle("Cats are chaotic evil: Ernie opines", "Ernie", list.size + 1))
-    list.addOne(new NewsArticle("Coffee stocks runs low in office. Chaos everywhere", "Big Bird", list.size + 1))
-
-    list
+    dataService.InitialiseNewsArticles()
   }
 
   def GetNews(): List[NewsArticle] = {
