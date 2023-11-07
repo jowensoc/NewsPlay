d76c8b4bb90d861981be1b571a0b2dc77f122049
John Owens <114502866+jowensoc@users.noreply.github.com>
2023-11-07 10:42:31 +0000

    Added admin view and also fixed typos (#17)

---
+++ NewsPlay/app/controllers/AdminController.scala
@@ -0,0 +1,49 @@
+package controllers
+
+import models.NewsArticle
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+import shared.SharedService
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class AdminController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+  /**
+   * Create an Action to render an HTML page.
+   *
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def index() = Action { implicit request: Request[AnyContent] =>
+    val list = services.NewsService.GetNews();
+    Ok(views.html.admin.index(list))
+  }
+
+  def createnews() = Action { implicit request: Request[AnyContent] =>
+    val newsArticle = SharedService.defaultNewsArticle()
+
+    Ok(views.html.admin.newsedit(newsArticle))
+  }
+
+  def editnews(articleID: Integer) = Action { implicit request: Request[AnyContent] =>
+    val optNewsArticle = services.NewsService.GetNewsByID(articleID)
+    var blankArticle = new NewsArticle("Not Found", "Not Found", 0)
+    blankArticle.content = "<p>This article could not be found</p>"
+
+    val newsArticle = optNewsArticle.getOrElse(blankArticle)
+
+    Ok(views.html.admin.newsedit(newsArticle))
+  }
+
+  def savenews() = Action { implicit request: Request[AnyContent] =>
+    Redirect(routes.AdminController.index())
+  }
+
+}
---
+++ NewsPlay/app/shared/SharedService.scala
@@ -0,0 +1,21 @@
+package shared
+
+import models.NewsArticle
+
+object SharedService {
+
+  def createNewsArticle(articleID: Int, title: String, reporter: String, content: String): NewsArticle = {
+    val newsArticle = new NewsArticle(title, reporter, articleID)
+    newsArticle.content = content
+
+    newsArticle
+  }
+
+  def defaultNewsArticle(): NewsArticle = {
+    val newsArticle = new NewsArticle("", "", 0)
+    newsArticle.content = ""
+
+    newsArticle
+  }
+
+}
