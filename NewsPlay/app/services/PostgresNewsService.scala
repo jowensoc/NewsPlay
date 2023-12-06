ec4f0861a52ef580fa5ee31e685bebbec2cc443e
John Owens <john.owens@opencastsoftware.com>
2023-12-06 15:21:58 +0000

    Add new database service

--- NewsPlay/app/services/PostgresNewsService.scala
+++ NewsPlay/app/services/PostgresNewsService.scala
@@ -1,6 +1,32 @@
 package services
 import models.{NewsArticle, Reporter, SearchParameters}
 
-class PostgresNewsService {
+class PostgresNewsService extends BaseNewsService {
+  override def GetNews(): List[NewsArticle] = {
+    null
+  }
 
+  override def GetNewsByID(articleID: Int): Option[NewsArticle] = {
+    null
+  }
+
+  override def GetNewsByReporterShortName(reporterName: String): List[NewsArticle] = {
+    null
+  }
+
+  override def SearchArticles(searchParameters: SearchParameters): List[NewsArticle] = {
+    null
+  }
+
+  override def GetListOfReporters(): List[Reporter] = {
+    null
+  }
+
+  override def DeleteNewsByID(articleID: Int): Boolean = {
+    false
+  }
+
+  override def SearchArticlesByPattern(searchParameters: SearchParameters): List[NewsArticle] = {
+    null
+  }
 }
